6f1703ba7899432c3375246be834ae4d25241fb8
pushkarjoshi19152 <73287201+pushkarjoshi19152@users.noreply.github.com>
2023-04-08 15:58:47 +0530

    profile and menu style fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from 'react';
 import './header.css';
 import { Container, Row } from 'reactstrap';
-import {  NavLink, useNavigate} from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import logo from '../../assets/images/eco-logo.png';
 import userIcon from '../../assets/images/user-icon.png';
@@ -10,7 +10,7 @@ import { useSelector } from 'react-redux';
 import useAuth from '../../custom-hooks/useAuth';
 import { Link } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
-import { auth } from '../../firebase.config'; 
+import { auth } from '../../firebase.config';
 import { toast } from 'react-toastify';
 
 const nav__links = [
@@ -36,9 +36,9 @@ const Header = () => {
     const headerRef = useRef(null);
 
     const menuRef = useRef(null);
-    const navigate=useNavigate();
-    const {currentUser}= useAuth()
-    const profileActionRef= useRef(null)
+    const navigate = useNavigate();
+    const { currentUser } = useAuth()
+    const profileActionRef = useRef(null)
 
     const stickyHeaderFunc = () => {
         window.addEventListener('scroll', () => {
@@ -53,12 +53,12 @@ const Header = () => {
     };
 
 
-    const logout=()=>{
+    const logout = () => {
 
-        signOut(auth).then(()=>{
+        signOut(auth).then(() => {
             toast.success('Logged out')
             navigate('/home')
-        }).catch(err=>{
+        }).catch(err => {
             toast.error(err.message)
         })
 
@@ -72,11 +72,11 @@ const Header = () => {
 
     const menuToggle = () => menuRef.current.classList.toggle('active__menu');
 
-    const navigateToCart=()=>{
+    const navigateToCart = () => {
         navigate('/cart');
     }
 
-    const toggleProfileActions=()=>profileActionRef.current.classList.toggle('show__profileActions');
+    const toggleProfileActions = () => profileActionRef.current.classList.toggle('show__profileActions');
 
 
     return (
@@ -89,64 +89,84 @@ const Header = () => {
                             <div>
                                 <h1>MultiMart</h1>
                             </div>
-                        </div>   
+                        </div>
                         <div className="navigation" ref={menuRef} onClick={menuToggle}>
                             <motion.ul className="menu">
-                                {   nav__links.map((item, index) => (
-                                     <li className="nav__item" key={index}>
-                                            <NavLink 
-                                            to={item.path} 
-                                            className={(navClass) => 
-                                            navClass.isActive ? 
-                                            'nav__active' : ''
+                                {nav__links.map((item, index) => (
+                                    <li className="nav__item" key={index}>
+                                        <NavLink
+                                            to={item.path}
+                                            className={(navClass) =>
+                                                navClass.isActive ?
+                                                    'nav__active' : ''
                                             }
-                                            >
-                                        {item.display}
+                                        >
+                                            {item.display}
                                         </NavLink>
-                                        </li>
+                                    </li>
                                 ))
                                 }
                             </motion.ul>
                         </div>
                         <div className="nav__icons">
-                            <span className="fav__icon">
-                                <i className="ri-heart-line"></i>
-                                <span className="badge">2</span>
-                            </span>
-                            <span className="cart__icon" onClick={navigateToCart}>
-                                <i className="ri-shopping-bag-line"></i>
-                                <span className="badge">{totalQuantity}</span>
-                            </span>
+                            <div>
+
+                                <span className="fav__icon">
+                                    <i className="ri-heart-line"></i>
+                                    <span className="badge">2</span>
+                                </span>
+
+                            </div>
+                            <div>
+
+
+                                <span className="cart__icon" onClick={navigateToCart}>
+                                    <i className="ri-shopping-bag-line"></i>
+                                    <span className="badge">{totalQuantity}</span>
+                                </span>
+
+                            </div>
+
 
                             <div className="profile" onClick={toggleProfileActions}>
-                                <motion.img whileTap={{ scale: 1.2 }} 
-                                src={currentUser ? currentUser.photoURL: userIcon}
-                                alt="" 
+                                <motion.img whileTap={{ scale: 1.2 }}
+                                    src={currentUser ? currentUser.photoURL : userIcon}
+                                    alt=""
                                 />
 
-                                <div 
-                                className="profile__action"
-                                ref={profileActionRef} 
+                                <div
+                                    className="profile__action"
+                                    ref={profileActionRef}
                                 >
-                                {currentUser ?(
-                                        <span onClick={logout}>Logout</span>
-                                        ):( 
-                                        <div className="d-flex align-items-center justify-content-center flex-column">
-                                        <Link to='/signup'>Signup /</Link>
-                                        <Link to='/login'>login</Link>
-                                        <Link to='/dashboard'>Dashboard</Link>
-                                    </div>
-                                )}
+                                    {currentUser ? (
+                                        <div>
+                                            <span onClick={logout}>Logout</span>
+                                        </div>
+                                    ) : (
+                                        <div>
+                                            <span>
+
+                                                <Link to='/signup'>Signup </Link>
+
+                                                <Link to='/login'>login</Link>
+
+
+
+                                                <Link to='/dashboard'>Dashboard</Link>
+                                            </span>
+                                        </div>
+                                    )}
+
+
                                 </div>
                             </div>
-                                
-                                <div className="mobile__menu">
-                                    <span onClick={menuToggle}>
+                            <div className="mobile__menu">
+                                <span className="fav__icon" onClick={menuToggle}>
                                     <i className="ri-menu-line"></i>
                                 </span>
-                                </div>
+                            </div>
                         </div>
-                    
+
                     </div>
                 </Row>
             </Container>

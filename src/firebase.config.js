72fcee7aa11eaf083bcb628407f69afa893faf0b
krishna-1902 <85438311+krishna-1902@users.noreply.github.com>
2023-04-18 21:28:49 +0530

    Removed Dead Code

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,14 +4,6 @@ import {getFirestore } from 'firebase/firestore'
 import {getStorage} from 'firebase/storage'
 import { getAnalytics } from "firebase/analytics";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC7OPx93-eYFMDfQp93XHZDgL3vZ7JmuZs",
-//   authDomain: "maltimart-bf751.firebaseapp.com",
-//   projectId: "maltimart-bf751",
-//   storageBucket: "maltimart-bf751.appspot.com",
-//   messagingSenderId: "761079785532",
-//   appId: "1:761079785532:web:1c910e4fbbdd90f93bfb39"
-// };
 
 const firebaseConfig = {
   apiKey: "AIzaSyCuOR6CtB7MusujSxIYURbrUS7pL2OZS0Q",

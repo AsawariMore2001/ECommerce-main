7d7c82a4003fcbe95529bfcc52f936d0f5517a66
Asawari More <asavarimp@gmail.com>
2023-05-03 20:39:52 +0530

    Reverted to previous changes of MyOrder PAge

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -1,37 +1,77 @@
 import React, { useState, useEffect } from 'react'
 import Helmet from '../components/Helmet/Helmet'
-import { Container, Col } from 'reactstrap'
-import { getAuth } from "firebase/auth";
+import { Container, Col, Row, Form, FormGroup } from 'reactstrap'
+import { Link } from 'react-router-dom'
+import { createUserWithEmailAndPassword, updateProfile, getAuth, onAuthStateChanged } from "firebase/auth";
+import { auth } from '../firebase.config';
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { storage } from '../firebase.config';
+import { toast } from 'react-toastify'
 import { db } from '../firebase.config';
-import { Link } from "react-router-dom";
-import { collection, getDocs } from 'firebase/firestore';
+
+import { setDoc, doc, updateDoc, collection, onSnapshot, getDocs, query } from 'firebase/firestore';
 import '../styles/myorders.css'
+import { async } from '@firebase/util';
+import { useNavigate } from 'react-router-dom';
 import { motion } from "framer-motion";
+import useGetDataNew from "../custom-hooks/useGetDataNew";
+import useAuth from "../custom-hooks/useAuth"
 
 const MyOrders = () => {
 
     const [ordersData, setOrdersData] = useState([]);
     const [loading, setLoading] = useState(false);   
+    const { currentUser } = useAuth()
+     // const collectionRef = doc(db, 'orders', currentUser.uid)
+    // const { data: products, loading } = useGetDataNew(collectionRef);
+    const handleClick = async () => {
+        // console.log("clicked");
+        // const snapShot = await getDocs(collection(db, 'neworder', currentUser.uid, "myorders"));
+        // snapShot.forEach((doc) => {
+        //     console.log(doc.data());
+        // })
+        console.log(ordersData);
 
+    }
+
+    
     useEffect(() => {
 
         const getData = async (uid) => {
 
+
+            // await onSnapshot(collection(db, 'orders', currentUser.uid, "myorders"), (snapshot) => {
+            //     setOrdersData(snapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })))
+            // })
             setLoading(true)
             const auth = getAuth();
 
             try {
 
                 const snapShot = await getDocs(collection(db, 'neworder', auth.currentUser.uid, "myorders"));
-                
+                // setOrdersData([]);
+                // const temp = []
+                // snapShot.forEach((doc) => {
+                //     temp.push({ ...doc.data(), id: doc.id });
+
+                // })
+                // setOrdersData(temp);
                 setOrdersData(snapShot.docs.map(doc => ({ ...doc.data(), id: doc.id })));
 
             } catch (error) {
                 console.log(error)
             }
 
+
+            // console.log(currentUser.uid)
+            // const snapShot = await getDocs(collection(db, 'neworder', currentUser.uid, "myorders"));
+            // setOrdersData(snapShot.docs.map(doc=>({...doc.data(),id:doc.id})));
+
+
             setLoading(false)
 
+            // setOrdersData(querySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id })))
+       
         }
         getData();
 
@@ -41,6 +81,7 @@ const MyOrders = () => {
         <section>{
             loading ? <Col lg='12' className='text-center' ><h5 className='fw-bold'>Loading Orders</h5></Col> :
                 <Container>
+                    <button onClick={handleClick}> check </button>
                     <table className='table border'>
                         <thead>
                             <tr>
@@ -75,6 +116,22 @@ const Tr = ({ outer_item }) => {
                 return <Trr item={item1} key={index} />
             })
         }
+        <tr>
+            <td><center>TotalQuantity: {outer_item.TotalQuantity}</center></td>
+            <td><center>TotalAmount: {outer_item.TotalAmount}</center></td>
+            <td></td>
+            <td></td>
+            <motion.td whileHover={{ scale: 1.2 }}><center>
+            <Link to="/ProductWarranty">
+                <motion.i
+                    whileTap={{ scale: 1.2 }}
+                    className="ri-shield-flash-line"
+                ></motion.i>{" "}
+            </Link>
+        </center>
+        </motion.td>
+
+        </tr>
     </>
     );
 };
@@ -88,15 +145,7 @@ const Trr = ({ item }) => {
         <td><center>{item.productName}</center></td>
         <td><center>₹{item.price}</center></td>
         <td><center>{item.quantity} </center></td>
-        <motion.td whileHover={{ scale: 1.2 }}><center>
-            <Link to="/ProductWarranty">
-                <motion.i
-                    whileTap={{ scale: 1.2 }}
-                    className="ri-shield-flash-line"
-                ></motion.i>{" "}
-            </Link>
-        </center>
-        </motion.td>
+       
 
     </tr>)
 }
